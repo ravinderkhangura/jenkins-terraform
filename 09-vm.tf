7b09b61042d837f09fab1d60fdec0c6028b306be
Ravinder Khangura <ravinderkhangura18@gmail.com>
2024-11-27 19:36:18 -0500

    terraform actions

--- 09-vm.tf
+++ 09-vm.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "myec2" {
   ami = "ami-0166fe664262f664c"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet1.id
-  key_name = "AWSHYD"
+  key_name = "MyKey"
   vpc_security_group_ids = [ aws_security_group.mysg.id ]
   associate_public_ip_address = true
   user_data = <<EOF
