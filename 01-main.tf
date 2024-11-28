40a06b10ad723fe77d9734413ac07fd7e26dabd8
Ravinder Khangura <ravinderkhangura18@gmail.com>
2024-11-27 19:06:07 -0500

    terraform innit

--- 01-main.tf
+++ 01-main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.76.0"
+      source = "hashicorp/aws"
+      version = "5.78.0"
     }
   }
   backend "s3" {
